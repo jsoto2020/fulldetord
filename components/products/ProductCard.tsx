f5c04aa35c132cb184ea510cc2f62d9997764d69
Juan Soto <juan.f.sotoc@gmail.com>
2023-02-23 08:56:29 -0400

    MODIFICACION API/ADMIN/PRODUCTS

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -15,8 +15,8 @@ export const ProductCard: FC<Props> = ({ product }) => {
 
     const productImage = useMemo(() => {
         return isHovered
-          ? `${ product.images[1] }`
-          : `${ product.images[0] }`;
+          ?  product.images[1]
+          :  product.images[0] ;
 
     }, [isHovered, product.images])
 
--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -48,7 +48,9 @@ const getProducts = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
     // TODO:
     const updatedProducts = products.map( product => {
         product.images = product.images.map( image => {
-            return image.includes('http') ? image : `${ process.env.HOST_NAME}${ image }`
+            return image
+            console.log('imagenes: ',image)
+         //   return image.includes('http') ? image : `${ process.env.HOST_NAME}${ image }`
         });
 
         return product;
