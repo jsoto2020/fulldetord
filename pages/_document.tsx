eb2e1f8b1760aad14920bfe630e00ccec1e16aca
Juan Soto <juan.f.sotoc@gmail.com>
2023-03-10 20:35:41 -0400

    MAS FAVICON

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,7 @@ class MyDocument extends Document {
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
                     />
-                     <link rel="shortcut icon" href="/favicon.ico" sizes="62x62" /> 
+                     <link rel="shortcut icon" href="/favicon.ico" sizes="100x100" /> 
                 </Head>
                 <body>
                     <Main />
