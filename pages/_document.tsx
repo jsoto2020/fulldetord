05226dad8b2557566c59a481b189d2ef6a63cc12
Juan Soto <juan.f.sotoc@gmail.com>
2023-03-10 20:31:38 -0400

    AUMENTE EL FAVICON

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,7 @@ class MyDocument extends Document {
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
                     />
-                     <link rel="shortcut icon" href="/favicon.ico" sizes="42x42" /> 
+                     <link rel="shortcut icon" href="/favicon.ico" sizes="62x62" /> 
                 </Head>
                 <body>
                     <Main />
